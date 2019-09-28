6dc738291bda74b877fe505519895f23451124a9
Annie <annie.yiyunc@gmail.com>
2019-09-27 21:48:57 -0700

    removed bootstrap script tags to fix $.ajax not a function error

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@ $(document).ready(
     function gifGenerater() {
         var animalList = ["penguin", "cat", "dog", "bird", "tuna", "kangaroo", "panda"];
 
+        // render buttons with animals from animalList 
         function buttonGenerator() {
             $(".buttons-div").empty();
             animalList.forEach(function (animal) {
@@ -12,9 +13,10 @@ $(document).ready(
 
             })
         };
-        
+
         buttonGenerator();
 
+        // When submit button clicked, take search input and make it into a button with the input as value
         $("#submit-button").on("click", function (e) {
             e.preventDefault();
             var searchTerm = $("#search-input").val().trim();
@@ -24,12 +26,25 @@ $(document).ready(
         })
 
         // when animal button click, call api and display 10 gif with its rating
-        // $(".animalButton").on("click", function(){
-        //     $.ajax(
-        //         url:
-        //         method: 
-        //     )
-        // })
+        $(".animalButton").on("click", function () {
+            var animal = $(this).attr("value");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                animal + "&api_key=CI5BOuYtH2R5TZWlbUOCcWdPBiaMSZ5V&limit=10";
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response);
+                var gifImage = response.data.images.original_still;
+                var newImage = $("<Image>");
+
+                newImage.attr("scr", gifImage);
+                $(".gif-div").append(newImage);
+
+            });
+
+        })
 
 
 
