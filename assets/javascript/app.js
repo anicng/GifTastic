2aec58c3ed8836a76beb6df94a9e93f0897b955c
Annie <annie.yiyunc@gmail.com>
2019-09-27 20:54:15 -0700

    When submit button hit, grab user input and display as button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,11 @@
 $(document).ready(
     function gifGenerater() {
         var animalList = ["penguin", "cat", "dog", "bird", "tuna", "kangaroo", "panda"];
-        var searchInput = $("search-input");
 
         function buttonGenerator() {
             $(".buttons-div").empty();
             animalList.forEach(function (animal) {
-                var newButtons = $("<button type='button' class='btn btn-outline-info'>");
+                var newButtons = $("<button type='button' class='btn btn-outline-info mr-2 animalButton'>");
                 newButtons.text(animal);
                 $(".buttons-div").append(newButtons);
 
@@ -14,9 +13,15 @@ $(document).ready(
         };
         buttonGenerator();
 
-        // $("#submit-button").on("click",function(){
+        $("#submit-button").on("click", function (e) {
+            e.preventDefault();
+            var searchTerm = $("#search-input").val().trim();
+            console.log(searchTerm);
+            animalList.push(searchTerm);
+            buttonGenerator();
+        })
 
-        // })
+
 
 
 
