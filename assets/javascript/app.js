cd5c5258b55e8762026b4af12fb097531db6fcb1
Annie <annie.yiyunc@gmail.com>
2019-09-27 20:26:02 -0700

    render array list buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,23 @@
 $(document).ready(
     function gifGenerater() {
-        
+        var animalList = ["penguin", "cat", "dog", "bird", "tuna", "kangaroo", "panda"];
+        var searchInput = $("search-input");
+
+        function buttonGenerator() {
+            $(".buttons-div").empty();
+            animalList.forEach(function (animal) {
+                var newButtons = $("<button type='button' class='btn btn-outline-info'>");
+                newButtons.text(animal);
+                $(".buttons-div").append(newButtons);
+
+            })
+        };
+        buttonGenerator();
+
+        // $("#submit-button").on("click",function(){
+
+        // })
+
 
 
     });
